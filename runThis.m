75d4f28504d60995a5e9f3914a6d8565be5ca135
Justin Alvey <jual6332@colorado.edu>
2016-10-23 13:35:42 -0600

    Re-Formatted code structure

--- runThis.m
+++ runThis.m
@@ -6,6 +6,18 @@
 %% Clear Everything
 clc; clear all; close all;
 
+%% Initial Declarations
+% Define all constants
+N = 1000;  % Number of panels in Vortex Panel Method
+Vinf = 25; % Freestream velocity condition
+alpha = 0; % Angle of Attack
+c = 1.0;   % Chord length
+
+% Define variables for the geometry of NACA 0012 airfoil
+m = 0.0/100.00; % Max camber
+p = 0.0/10.0; % Location of max camber
+t = 12.0/100; % Thickness
+
 %% Capture Example Airfoil Data from Fortran code
 % Airfoil mid-point coordinates from Fortran code
 % Used as experimental verification of code early on
@@ -17,12 +29,6 @@ exYb = [0.0 -0.005 -0.017 -0.033 -0.042 -0.033 0.0 0.045 0.076 0.072 0.044 0.013
 %given aifoil data
 
 %% Problem #2
-% Define variables for the geometry of NACA 0012 airfoil
-N = 500; % Number of panels in Vortex Panel Method
-m = 0.0/100.00; % Max camber
-p = 0.0/10.0; % Location of max camber
-t = 12.0/100; % Thickness
-c = 1.0; % Chord length
 tic;
 
 % Call function to get X,Y locations for the airfoil
@@ -34,24 +40,10 @@ xlabel('Xb')
 ylabel('Yb')
 
 % Determine cl for NACA 0012 with different numbers of panels
-% Calculate cl for N = 500
-alpha = 0;
-%[c_l] = Vortex_Panel(Xb,Yb,25,alpha) % Call Vortex_Panel function
-%c_lTest(1) = c_l;
-
-% Determine cl for NACA 0012 with different numbers of panels
 % Calculate cl for N = 1000
-N = 5000;
 [Xb,Yb] = NACA_Airfoil(m,p,t,c,N); % Call airfoil function again with new
-[c_l] = Vortex_Panel(Xb,Yb,25,alpha) % Call Vortex_Panel function
-c_lTest(2) = c_l;
+[c_l] = Vortex_Panel(Xb,Yb,Vinf,alpha) % Call Vortex_Panel function
 
-% Determine cl for NACA 0012 with different numbers of panels
-% Calculate cl for N = 10000
-%N = 10000; 
-%[Xb,Yb] = NACA_Airfoil(m,p,t,c,N); % Call airfoil function again with new
-%[c_l] = Vortex_Panel(Xb,Yb,25,alpha); % Call Vortex_Panel function
-%c_lTest(3) = c_l;
 
 % Determine cl for NACA 0012 with varying angles of attack (Nominal: N = 10000)
 %alpha = [-5 0 5 10];
