8c9e9c74a40bc684c146018304f2639d3d41b5be
Justin Alvey <jual6332@colorado.edu>
2016-10-28 04:47:22 -0600

    Calls new Problem3 function

--- runThis.m
+++ runThis.m
@@ -5,10 +5,12 @@
 
 %% Clear Everything
 clc; clear; close all;
+fprintf('Here are tabulated results:\n')
+fprintf('--------------------------\n')
 
 %% Initial Declarations
 % Define all constants
-N = 1500;  % Number of panels in Vortex Panel Method
+N = 250;  % Number of panels in Vortex Panel Method
 Vinf = 25; % Freestream velocity condition
 alpha = 0; % Angle of Attack
 c = 1.0;   % Chord length 
@@ -18,21 +20,22 @@ m = 0.0/100.00; % Max camber
 p = 0.0/10.0; % Location of max camber
 t = 12.0/100; % Thickness
 
-%% Capture Example Airfoil Data from Fortran code
+
+%% Problem #1
+%cl = Vortex_Panel(exXb,exYb,25,8); % Call Vortex_Panel function to test given aifoil data
+% Capture Example Airfoil Data from Fortran code
 % Airfoil mid-point coordinates from Fortran code
 % Used as experimental verification of code early on
 exXb = [1.0 0.933 0.750 0.500 0.250 0.067 0.0 0.067 0.250 0.500 0.750 0.933 1.0]; % X-coordinate midpoints for Vpanel Method
 exYb = [0.0 -0.005 -0.017 -0.033 -0.042 -0.033 0.0 0.045 0.076 0.072 0.044 0.013 0.0]; % Y-coordinate midpoints for Vpanel Method
 
-%% Problem #1
-%cl = Vortex_Panel(exXb,exYb,25,8); % Call Vortex_Panel function to test given aifoil data
 
 %% Problem #2
 % Call function to get X,Y locations for the airfoil
 [Xb,Yb] = NACA_Airfoil(m,p,t,c,N); % NACA 0012 Airfoil
 
 % Plot the airfoil geometry based on the data
-%figure;
+figure;
 plot(Xb, Yb) % Plot airfoil based on the data
 xlabel('Xb')
 ylabel('Yb')
@@ -46,31 +49,23 @@ for i=1:length(alpha_vary)
     printLift = strcat('cl for ',AirfoilName,' (',num2str(alpha_vary(i)), ' deg) is %0.4f\n');
     fprintf(printLift,c_l);
 end
-%dcl_da = (CL(end) - CL(1)) / (pi*(alpha_vary(end) - alpha_vary(1))/100); % Calculate dcl/dalpha
 
 % Plot cl vs alpha for [-5 0 5 10]
+figure;
 plot(alpha_vary,CL,'Linewidth',2)
 hold on
 plot(alpha_vary,CL,'ko','Linewidth',2)
 title('NACA 0012: Sectional Coefficient of Lift vs \alpha')
 xlabel('\alpha')
 ylabel('c_{l}')
-
-%% Problem #3
-% Determine cl vs alpha for a number of airfoils)
-alpha_vary = linspace(-5,10);
-for i=1:length(alpha_vary)
-    [c_l] = Vortex_Panel(Xb,Yb,Vinf,alpha_vary(i)); % Call Vortex_Panel function
-    CL(i) = c_l; % Store this value for c_l
-end
-
-% Plot cl vs alpha for linearly space range of alpha
-plot(alpha_vary,CL,'Linewidth',2)
-hold on
-plot(alpha_vary,CL,'ko','Linewidth',2)
-title('NACA 0012: Coefficient of Lift vs \alpha')
-xlabel('\alpha')
-ylabel('c_{l}')
+hold off;
 
 
-%% problem #4
+%% Problem #3 - Determine cl vs alpha for a number of airfoils
+fprintf('\n')
+problem3(true,N,Vinf,m,p,t,c); % Call function for Problem 3
+% This function takes care of all calculations by calling plotCLvsAlpha.
+% This function returns results to be plotted
+
+
+
